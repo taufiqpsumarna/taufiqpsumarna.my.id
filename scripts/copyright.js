1a145dcbc1d0fe9cd7307c211bf0a60e1bd8fb6f
idstar-taufiqps <taufiq.permana@idstar.co.id>
2024-09-10 21:52:12 +0700

    remove scripts

--- scripts/copyright.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const year = new Date().getFullYear();
-    const copyrightElement = document.getElementById("copyright-year");
-    if (copyrightElement) {
-        copyrightElement.textContent = year;
-    }
-});
